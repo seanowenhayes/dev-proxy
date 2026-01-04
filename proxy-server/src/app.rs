56a975f61cd1e431a195696911935adb7a4db52e
Sean Hayes <seanowenhayes@gmail.com>
2026-01-04 12:21:16 +0100

    Refactor proxy and app into seperate modules both exposing a main method

--- proxy-server/src/app.rs
+++ proxy-server/src/app.rs
@@ -8,10 +8,24 @@ use futures_util::stream::{self, Stream};
 use std::{convert::Infallible, time::Duration};
 use tokio_stream::StreamExt as _;
 
-pub fn app() -> Router {
+use tower_http::cors::{Any, CorsLayer};
+
+fn app() -> Router {
     Router::new()
         .route("/", get(|| async { "Hello, Axums!" }))
         .route("/sse", get(sse_handler))
+        .layer(
+            // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
+            // for more details
+            //
+            // pay attention that for some request types like posting content-type: application/json
+            // it is required to add ".allow_headers([http::header::CONTENT_TYPE])"
+            // or see this issue https://github.com/tokio-rs/axum/issues/849
+            CorsLayer::new()
+                .allow_origin(Any) // or specific origins
+                .allow_methods(Any)
+                .allow_headers(Any),
+        )
 }
 
 async fn sse_handler(
@@ -23,7 +37,7 @@ async fn sse_handler(
     //
     // You can also create streams from tokio channels using the wrappers in
     // https://docs.rs/tokio-stream
-    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+    let stream = stream::repeat_with(|| Event::default().data("{\"message\": \"hi!\"}"))
         .map(Ok)
         .throttle(Duration::from_secs(1));
 
@@ -32,4 +46,18 @@ async fn sse_handler(
             .interval(Duration::from_secs(1))
             .text("keep-alive-text"),
     )
+}
+
+pub async fn main() {
+    let app = app();
+
+    let port: u16 = std::env::var("AXUM_SERVER_PORT")
+        .unwrap_or_else(|_| "3030".to_string())
+        .parse()
+        .expect("Failed to parse axum PORT");
+
+    let address = format!("0.0.0.0:{}", port);
+    println!("Address {}", address);
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
--- proxy-server/src/main.rs
+++ proxy-server/src/main.rs
@@ -1,23 +1,11 @@
 use dotenv::dotenv;
 
-use crate::app::app;
-
 mod app;
 mod proxy;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    // proxy::main().await;
-    let app = app();
 
-    let port: u16 = std::env::var("AXUM_SERVER_PORT")
-        .unwrap_or_else(|_| "3030".to_string())
-        .parse()
-        .expect("Failed to parse axum PORT");
-
-    let address = format!("0.0.0.0:{}", port);
-    println!("Address {}", address);
-    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let (_proxy, _app) = tokio::join!(proxy::main(), app::main());
 }
