1c098cfaf45a0f9acbe0bca212589f82113ebaea
Sean Hayes <seanowenhayes@yandex.com>
2026-03-06 16:56:59 +0100

    Add testing on the js side

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
     plugins: [react()],
     build: {
